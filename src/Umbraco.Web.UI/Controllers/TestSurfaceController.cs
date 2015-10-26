9648cfd4dd083fcc2b9e1797db54bc1bd00476e3
Shannon <sdeminick@gmail.com>
2015-10-26 23:34:17 +0100

    Shows that model state works.

--- src/Umbraco.Web.UI/Controllers/TestSurfaceController.cs
+++ src/Umbraco.Web.UI/Controllers/TestSurfaceController.cs
@@ -2,15 +2,22 @@
 using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Mvc.ModelBinding;
 using Microsoft.Framework.Primitives;
+using Umbraco.ViewModels;
 using Umbraco.Web.Controllers;
 
 namespace Umbraco.Controllers
 {
     public class TestSurfaceController : SurfaceController
     {
-        public IActionResult DoThis()
+        public IActionResult DoThis(UmbracoFormTest model)
         {
-            this.ViewBag.warning = "This is a warning from view data!";
+            if (ModelState.IsValid == false)
+            {
+                this.ViewBag.warning = "This is a warning from view data!";
+                return CurrentUmbracoPage();
+            }
+
+            this.ViewBag.warning = "Success!";
 
             ModelState.SetModelValue("test",
                 new ValueProviderResult(new StringValues(new [] { "YAY", "YAY" }), CultureInfo.CurrentCulture));
---
+++ src/Umbraco.Web.UI/ViewModels/UmbracoFormTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Umbraco.ViewModels
+{
+    public class UmbracoFormTest
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
