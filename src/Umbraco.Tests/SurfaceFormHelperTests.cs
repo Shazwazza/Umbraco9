a20d95619c65eaf315ac93c2fa29305c1859341f
Shannon <sdeminick@gmail.com>
2015-11-03 19:53:11 +0100

    another test

--- src/Umbraco.Tests/SurfaceFormHelperTests.cs
+++ src/Umbraco.Tests/SurfaceFormHelperTests.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Microsoft.AspNet.DataProtection;
 using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Http.Internal;
+using Microsoft.AspNet.Routing;
 using Microsoft.Framework.Logging;
 using Microsoft.Framework.Primitives;
 using Moq;
+using Umbraco.Web;
 using Umbraco.Web.Routing;
 using Xunit;
 
@@ -12,6 +17,37 @@ namespace Umbraco.Tests
 {
     public class SurfaceFormHelperTests
     {
+        [Fact]
+        public void GetFormInfo()
+        {
+            var protector = new Mock<IDataProtector>();
+            var protectorProvider = new Mock<IDataProtectionProvider>();
+            protectorProvider.Setup(x => x.CreateProtector(It.IsAny<string>())).Returns(protector.Object);
+            var surfaceRouteParams = $"{SurfaceFormHelper.ReservedAdditionalKeys.Controller}=mycontroller&{SurfaceFormHelper.ReservedAdditionalKeys.Action}=myaction&{SurfaceFormHelper.ReservedAdditionalKeys.Area}=myarea";
+            protector.Setup(dataProtector => dataProtector.Protect(It.IsAny<byte[]>())).Returns(Encoding.UTF8.GetBytes(surfaceRouteParams));
+            protector.Setup(dataProtector => dataProtector.Unprotect(It.IsAny<byte[]>())).Returns(Encoding.UTF8.GetBytes(surfaceRouteParams));
+            var helper = new SurfaceFormHelper(Mock.Of<ILoggerFactory>(), protectorProvider.Object);
+            var request = new Mock<HttpRequest>();
+            request.Setup(httpRequest => httpRequest.Method).Returns("GET");
+            request.Setup(httpRequest => httpRequest.Query).Returns(
+                new FormCollection(new Dictionary<string, StringValues>
+                {
+                    {"ufprt", Convert.ToBase64String(Encoding.UTF8.GetBytes(surfaceRouteParams))}
+                }));
+
+            var httpCtxAccessor = new Mock<IHttpContextAccessor>();
+            var httpContext = new Mock<HttpContext>();
+            httpContext.Setup(context => context.Request).Returns(request.Object);
+            httpCtxAccessor.Setup(accessor => accessor.HttpContext).Returns(httpContext.Object);            
+            var routeCtx = new RouteContext(httpContext.Object);
+
+            var result = helper.GetFormInfo(routeCtx);
+
+            Assert.Equal("mycontroller", result.ControllerName);
+            Assert.Equal("myaction", result.ActionName);
+            Assert.Equal("myarea", result.Area);
+        }
+
         [Fact]
         public void GetRequestSurfaceToken_Post()
         {
